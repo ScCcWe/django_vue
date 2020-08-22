ca6221cf4a86f157912c49807a1993eb9dfe9495
ScCcWe <1229659207@qq.com>
2020-08-23 07:58:27 +0800

    new push

---
+++ djvue/appfront/src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios);
+Vue.prototype.$axios = axios;
+Vue.use(ElementUi)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
